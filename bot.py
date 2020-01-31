c08db0b4cfc646bb10b5f274df01a8c64a83d5e6
Eugene <gigantina.ru@yandex.ru>
2020-01-31 23:01:23 +0300

    31.01.2020

---
+++ bot.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import telebot
+import config
+import functions
+
+from telebot import types
+
+weather_ = False
+simple_ = False
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("5 Новостей")
+    item2 = types.KeyboardButton("Теоремы геометрии и алгебры")
+    item3 = types.KeyboardButton('Орел и Решка')
+    item4 = types.KeyboardButton('Погода')
+    markup.add(item1, item2, item3, item4)
+
+    bot.send_message(message.chat.id,
+                     'Привет, {0.first_name}!\n Я Пифа, кабарга. Создана для выполнения крайне необходимых задач) Ну а если хочешь узнать, кто это такая ваша кабарга, то вот ссылка: https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%B1%D0%B0%D1%80%D0%B3%D0%B0'.format(
+                         message.from_user), parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def dialog(message):
+    global weather_
+    if message.text == '5 Новостей':
+        for i in functions.parser():
+            bot.send_message(message.chat.id, i)
+
+    elif message.text == 'Орел и Решка':
+        choice = functions.orel()
+        bot.send_message(message.chat.id, choice)
+
+    elif message.text == 'Простое число':
+        simple_ = True
+        bot.send_message(message.chat.id, 'Введи число')
+
+    elif message.text == 'Погода':
+        weather_ = True
+        bot.send_message(message.chat.id, 'Введи город')
+
+    elif message.text != 'Погода' and weather_:
+        weather_ = False
+        gorod = str(message.text)
+        temperature = functions.weather(gorod)
+        bot.send_message(message.chat.id, temperature)
+
+    elif message.text != 'Простое число' and simple_:
+        number = str(message.text)
+        number = functions.simple(number)
+        simple_ = False
+        bot.send_message(message.chat.id, number)
+
+    else:
+        bot.send_message(message.chat.id, 'Мой глупый автор не прописал диалоги 😢')
+
+
+bot.polling(none_stop=True, interval=0)
---
+++ functions.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import random
+from pyowm import OWM
+
+def parser():
+    r = requests.get('https://ria.ru/')
+    html = BS(r.content, 'html.parser')
+    i = 0
+    news = []
+    for j in html.select('.cell-list__item'):
+        i += 1
+        if i <= 5:
+            title = j.select('.cell-list__item-title')
+            news.append(title[0].text)
+        else:
+            return news
+
+
+def weather(gorod):
+    API_key = config.PYOWN
+    owm = OWM(API_key)
+    try:
+        obs = owm.weather_at_place(gorod)
+        w = obs.get_weather()
+        temperature = w.get_temperature('celsius')['temp']
+        if temperature > 0:
+            temp = 'Температура в г. {} равна {}{}°C'.format(gorod, '+', temperature)
+        else:
+            temp = 'Температура в г. {} равна {}°C'.format(gorod, temperature)
+    except:
+        temp = 'Такого города не существует'
+    return temp
+
+
+def orel():
+    decision = random.choice(['Орел', 'Решка'])
+    return decision
+
+
+def simple(x):
+    try:
+        x = int(x)
+        dividers = [str(i)  for i in range(1, x + 1) if x % i == 0]
+        if len(dividers) > 2:
+            dividers = ', '.join(dividers)
+        else:
+            dividers = 'Простое число'
+    except:
+        dividers = 'Это не число'
+    return dividers
